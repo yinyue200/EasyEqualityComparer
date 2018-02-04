e61cc05fd2862216b9af50e7e041a520d2613407
yinyue200 <yinyue200@outlook.com>
2018-02-04 18:53:33 +0800

    性能优化

--- OperatorEqualityComparer/OperatorEqualityComparer.cs
+++ OperatorEqualityComparer/OperatorEqualityComparer.cs
@@ -4,33 +4,33 @@ using System.Text;
 
 namespace Yinyue200.EasyComparer
 {
-    internal class EasyEqualityComparer<T> : IEqualityComparer<T>
-    {
-        public EasyEqualityComparer(Func<T, T, bool> func)
-        {
-            this.func = func;
-        }
-        readonly Func<T, T, bool> func;
-        public bool Equals(T x, T y) => func(x, y);
-
-        public int GetHashCode(T obj) => EqualityComparer<T>.Default.GetHashCode(obj);
-    }
-    internal class EasyHashCodeEqualityComparer<T> : IEqualityComparer<T>
-    {
-        public EasyHashCodeEqualityComparer(Func<T, T, bool> func, Func<T, int> func2)
-        {
-            this.func = func;
-            this.func2 = func2;
-        }
-        readonly Func<T, T, bool> func;
-        readonly Func<T, int> func2;
-
-        public bool Equals(T x, T y) => func(x, y);
-
-        public int GetHashCode(T obj) => func2(obj);
-    }
     public static class EasyComparerBuilder
     {
+        sealed class EasyEqualityComparer<T> : IEqualityComparer<T>
+        {
+            public EasyEqualityComparer(Func<T, T, bool> func)
+            {
+                this.func = func;
+            }
+            readonly Func<T, T, bool> func;
+            public bool Equals(T x, T y) => func(x, y);
+
+            public int GetHashCode(T obj) => EqualityComparer<T>.Default.GetHashCode(obj);
+        }
+        sealed class EasyHashCodeEqualityComparer<T> : IEqualityComparer<T>
+        {
+            public EasyHashCodeEqualityComparer(Func<T, T, bool> func, Func<T, int> func2)
+            {
+                this.func = func;
+                this.func2 = func2;
+            }
+            readonly Func<T, T, bool> func;
+            readonly Func<T, int> func2;
+
+            public bool Equals(T x, T y) => func(x, y);
+
+            public int GetHashCode(T obj) => func2(obj);
+        }
         public static IEqualityComparer<T> BuildEqualMethod<T>(Func<T, T, bool> func) => new EasyEqualityComparer<T>(func);
         public static IEqualityComparer<T> BuildEqualAndGetHashCodeMethod<T>(Func<T, T, bool> func, Func<T, int> func2) => new EasyHashCodeEqualityComparer<T>(func,func2);
 
